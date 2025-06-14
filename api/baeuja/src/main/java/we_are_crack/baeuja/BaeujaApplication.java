8c5bcab4b313afbd5018209a51213328c14855f5
on1ystar <tjdwls0607@naver.com>
2025-06-14 16:40:37 +0900

    init

---
+++ api/baeuja/src/main/java/we_are_crack/baeuja/BaeujaApplication.java
@@ -0,0 +1,13 @@
+package we_are_crack.baeuja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeujaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeujaApplication.class, args);
+	}
+
+}
---
+++ api/baeuja/src/test/java/we_are_crack/baeuja/BaeujaApplicationTests.java
@@ -0,0 +1,13 @@
+package we_are_crack.baeuja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeujaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
