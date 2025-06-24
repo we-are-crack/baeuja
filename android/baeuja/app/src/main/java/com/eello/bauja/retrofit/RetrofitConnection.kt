f1e15dde32ad946b879d13176ed52434a3a2c558
jongseong <eello.bdev@gmail.com>
2025-06-24 16:25:05 +0900

    Feat : init project

---
+++ android/baeuja/app/src/main/java/com/eello/bauja/retrofit/RetrofitConnection.kt
@@ -0,0 +1,23 @@
+package com.eello.bauja.retrofit
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitConnection {
+
+    companion object {
+        private val BASE_URL = "https://api.baeuja.xyz/"
+        private var INSTANCE: Retrofit? = null
+
+        fun getInstance(): Retrofit {
+            if (INSTANCE == null) {
+                INSTANCE = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+
+            return INSTANCE!!
+        }
+    }
+}
