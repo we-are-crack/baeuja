5f3dd54045864b1268f5425d2168ccc1aad691d4
jongseong <eello.bdev@gmail.com>
2025-08-11 16:02:15 +0900

    Feat : Loading 화면 추가

---
+++ android/baeuja/app/src/main/java/com/eello/baeuja/ui/component/LoadingComponent.kt
@@ -0,0 +1,52 @@
+package com.eello.baeuja.ui.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.eello.baeuja.ui.theme.BaujaTheme
+
+@Composable
+fun LoadingComponent(
+    message: String = "Loading, please wait..."
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(
+            color = Color.Gray,
+            strokeWidth = 4.dp,
+            modifier = Modifier
+                .height(48.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = message,
+            style = MaterialTheme.typography.bodyLarge,
+            color = Color.Gray
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewLoadingComponent() {
+    BaujaTheme {
+        LoadingComponent()
+    }
+}
